647ac51afa9c119345f53b61c9d7f919d55f4f07
Just-A-Fool <aedanscarn@gmail.com>
2019-09-25 09:14:26 -0600

    first three excersises

--- scripts/index.js
+++ scripts/index.js
@@ -1,8 +1,11 @@
+import store from './store.js';
+import item from './item.js';
 import shoppingList from './shopping-list.js';
 
 const main = function () {
   shoppingList.bindEventListeners();
   shoppingList.render();
+  
 };
 
 $(main);
---
+++ scripts/store.js
@@ -0,0 +1,8 @@
+import item from './item.js';
+const items = [];
+let hideCheckedItems = false;
+
+export default {
+    items,
+    hideCheckedItems
+};
